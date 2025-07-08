81c4231cdd09fe9a74bbf4ec613f276b91c9bc31
atwanzireT <tatwanzire@gmail.com>
2025-07-08 11:31:33 +0300

    fixing the users

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.ADMIN,
+        default=Role.STAFF,
     )
 
     USERNAME_FIELD = 'email'
